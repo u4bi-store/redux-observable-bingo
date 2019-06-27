09b39032b934db345bf54e398347281c37ce0d04
u4bi <rootcode10@gmail.com>
2019-06-28 05:02:04 +0900

    feat(constants): Added constants for redux actions.

---
+++ src/store/constants/index.js
@@ -0,0 +1,21 @@
+/* app */
+
+export const ON_GAME = '[app] 🚀 onGame'
+export const ON_RESULT = '[app] 🚀 onResult'
+
+export const ADD_PLAYER = '[app] 🎁 addPlayer'
+export const SET_TURN = '[app] 🎁 setTurn'
+
+export const SET_BINGO = '[app] 🎁 setBingo'
+export const CLEAR_BINGO = '[app] 🎁 clearBingo'
+export const RESULT_BINGO = '[app] 🎁 resultBingo'
+
+/* board */
+export const ON_BOARD = '[board] 🚀 onBoard'
+export const SET_BOARD = '[board] 🎁 setBoard'
+
+export const ON_CELL = '[board] 🚀 onCell'
+export const ON_CELL_SUCCESS = '[board] 🎁 onCellSuccess'
+export const ON_CELL_FAILURE = '[board] 🎁 onCellFailure'
+
+export const ON_BINGO = '[board] 🚀 onBingo'
